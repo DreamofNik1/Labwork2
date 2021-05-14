f362ea2d9c0f2000c781999c3145a4177465249f
DreamofNik1 <81799391+DreamofNik1@users.noreply.github.com>
2021-05-14 16:56:35 +0300

    Update main.cpp

--- пример-2/main.cpp
+++ пример-2/main.cpp
@@ -4,21 +4,21 @@
 #include "AlphaCipher.h"
 #include <locale>
 using namespace std;
-void check(const wstring& Text, const int & key)
+ voidcheck(const wstring& Text, const int & key)
 {
-    wstring s=Text;
+ wstring s=Text;
     try {
-        PerestanCipher skey(key);
-        wcout<<skey.CoderAlphaCipher(key, s)<<endl;
-        wstring g=skey.CoderAlphaCipher(key, s);
-        wcout<<skey.DecoderAlphaCipher(key,g)<<endl;
-    } catch (const cipher_error & e) {
-        cerr<<"Error: "<<e.what()<<endl;
+ AlphaCipher skey(key);
+ wcout<<skey.CoderAlphaCipher(key, s)<<endl;
+ wstring g=skey.CoderAlphaCipher(key, s);
+ wcout<<skey.DecoderAlphaCipher(key,g)<<endl;
+ } catch (const cipher_error & e) {
+ cerr<<"Error: "<<e.what()<<endl;
     }
 }
 int main()
 {
-    locale loc("ru_RU.UTF-8");
+ locale loc("ru_RU.UTF-8");
     locale::global(loc);
     check(L"PRograMmiroVANie",8);
     check(L"PRograMmiroVANie",10);
