b8607ec10afc303daa4991f14a7c00724d7273ca
DreamofNik1 <81799391+DreamofNik1@users.noreply.github.com>
2021-05-14 16:49:19 +0300

    Create main.cpp

---
+++ example-2/main.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <iostream>
+#include <cctype>
+#include "AlphaCipher.h"
+#include <locale>
+using namespace std;
+void check(const wstring& Text, const int & key)
+{
+    wstring s=Text;
+    try {
+        AlphaCipher skey(key);
+        wcout<<skey.CoderPerestanCipher(key, s)<<endl;
+        wstring g=skey.CoderPerestanCipher(key, s);
+        wcout<<skey.DecoderPerestanCipher(key,g)<<endl;
+    } catch (const cipher_error & e) {
+        cerr<<"Error: "<<e.what()<<endl;
+    }
+}
+int main()
+{
+    locale loc("ru_RU.UTF-8");
+    locale::global(loc);
+    check(L"PRograMmiroVANie",8);
+    check(L"PRograMmiroVANie",10);
+    check(L"LDFHFEUFHWBCD",5);
+    return 0;
+}
